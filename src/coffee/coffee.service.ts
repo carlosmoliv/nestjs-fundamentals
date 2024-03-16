c23ce8be341b49f657fea564f5a804f1efb07e25
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-16 11:45:39 -0300

    feat: add custom NotFoundException

--- src/coffee/coffee.service.ts
+++ src/coffee/coffee.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +22,9 @@ export class CoffeeService {
   }
 
   findOne(id: string): Coffee {
-    return this.coffees.find((coffee) => coffee.id === +id);
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+    if (!coffee) throw new NotFoundException(`Coffee #${id} not found`);
+    return coffee;
   }
 
   create(coffee: Coffee): void {
