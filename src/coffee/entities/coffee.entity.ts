6c646266ba704b94477a11afe8d2cae5491043a5
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-16 17:12:11 -0300

    feat: add typeorm decorators to Coffee entity

--- src/coffee/entities/coffee.entity.ts
+++ src/coffee/entities/coffee.entity.ts
@@ -1,6 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Coffee {
+  @Column()
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
