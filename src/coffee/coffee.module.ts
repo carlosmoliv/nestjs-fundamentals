b3a64ba8d497e7746d6693742c7bd2f26075ffdb
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-16 18:23:50 -0300

    feat: add typeorm module to the coffee module imports

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
 
-@Module({ controllers: [CoffeeController], providers: [CoffeeService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
+  controllers: [CoffeeController],
+  providers: [CoffeeService],
+})
 export class CoffeeModule {}
