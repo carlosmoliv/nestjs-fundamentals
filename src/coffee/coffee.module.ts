55604934d9c853da8bfb30de4d0b932a1bae2ad6
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-17 11:04:41 -0300

    feat: add Flavor entity and establish a many-to-many relationship with Coffee

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -3,9 +3,10 @@ import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeeController],
   providers: [CoffeeService],
 })
--- src/coffee/coffee.service.ts
+++ src/coffee/coffee.service.ts
@@ -13,11 +13,14 @@ export class CoffeeService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({ relations: ['flavors'] });
   }
 
   async findOne(id: number) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id },
+      relations: ['flavors'],
+    });
     if (!coffee) throw new NotFoundException(`Coffee #${id} not found`);
     return coffee;
   }
